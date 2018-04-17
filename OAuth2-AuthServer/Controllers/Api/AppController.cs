abd866af7a14519d9a48c67c9023464ca9955989
jsbass <bass@ou.edu>
2018-04-16 20:23:04 -0500

    Added API Framework. No Implementation

---
+++ OAuth2-AuthServer/Controllers/Api/AppController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OAuth2_AuthServer.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/app")]
+    [UserAuth]
+    public class AppController : Controller
+    {
+        /// <summary>
+        /// Let's a user create an app
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult> Create(object app)
+        {
+            //Check properties on app model
+            //Store
+            //Return result with app id
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Let's a user edit the details of an app
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("{id}")]
+        public async Task<ActionResult> Edit(int id, object app)
+        {
+            //Check user owns app
+            //Check properties on app model
+            //Store
+            //Return result
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Let's a user view the details of an app (includes clients and scopes)
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            //Check user owns app
+            //Return result
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Let's a user delete an app
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            //Check user owns app
+            //Remove from storage
+            //Return result
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Let's a user or a resource server list the tokens for an app
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{id}/tokens")]
+        [BasicAuth]
+        public async Task<ActionResult> ListTokens(int id)
+        {
+            //Check user owns app
+            //get list of tokens
+            //return
+            throw new NotImplementedException();
+        }
+        #region scopes
+        /// <summary>
+        /// Let's a user add a scope to an app
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="scope"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("{id}/scope")]
+        public async Task<ActionResult> AddScope(int id, object scope)
+        {
+            //Check user owns app
+            //check scope name doesn't exist
+            //store scope
+            //return result
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Let's a user remove a scope from an app
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("{id}/scope/{name}")]
+        public async Task<ActionResult> DeleteScope(int id, string name)
+        {
+            //Check user owns app
+            //check scope name exists
+            //remove scope from any existing clients
+            //remove scope from app
+            //return result
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region clients
+        /// <summary>
+        /// Let's a user add a client for an app
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("{id}/client")]
+        public async Task<ActionResult> AddClient(int id, object client)
+        {
+            //Check user owns app
+            //Generate client credentials
+            //Store
+            //return result
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Let's a user remove a client from an app
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="client_id"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [Route("{id}/client/{client_id}")]
+        public async Task<ActionResult> DeleteClient(int id, string client_id)
+        {
+            //Check user owns app
+            //check client_id exists
+            //Remove
+            //return result
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Let's a user or resource server view the list of access tokens for a client
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="client_id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [BasicAuth]
+        [Route("{id}/client/{client_id}/tokens")]
+        public async Task<ActionResult> ListClientTokens(int id, string client_id)
+        {
+            //Check user owns app
+            //check client_id exists
+            //return list of tokens
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ OAuth2-AuthServer/Controllers/Api/TokenController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OAuth2_AuthServer.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/token")]
+    public class TokenController : Controller
+    {
+        /// <summary>
+        /// Let's client get their own access token
+        /// client credentials can either be passed in body or in basic header
+        /// </summary>
+        /// <param name="grant_type"></param>
+        /// <param name="client_id"></param>
+        /// <param name="client_secret"></param>
+        /// <param name="scope"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult> Post(string grant_type, string client_id = "", string client_secret = "", string scope = "")
+        {
+            //Verify 1 but not both set of credentials exist (Basic or post body)
+            //Verify grant_type
+            //Verify credentials
+            //Generate Access token
+            //Store
+            //return result
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Let's a resource server get an access token for a client
+        /// </summary>
+        /// <param name="grant_type"></param>
+        /// <param name="client_id"></param>
+        /// <param name="client_secret"></param>
+        /// <param name="scope"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [BasicAuth]
+        [Route("/for_client")]
+        public async Task<ActionResult> PostForClient(string grant_type, string client_id, string client_secret, string scope = "")
+        {
+            //Verify grant_type
+            //Verify client_id exists for app
+            //Verify client credentials
+            //Generate access token
+            //Store
+            //return result
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Used by resource server to check access token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [BasicAuth]
+        [Route("")]
+        public async Task<ActionResult> Introspect(string token)
+        {
+            //Verify app owns token
+            //return result
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Let's a user or resource server revoke an access token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [HttpDelete]
+        [BasicAuth]
+        [UserAuth]
+        [Route("")]
+        public async Task<ActionResult> Revoke(string token)
+        {
+            //Verify app or person's app owns token
+            //Remove
+            //return Result
+            throw new NotImplementedException();
+        }
+    }
+}
